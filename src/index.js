01b12ef56ec9488261dae19f3c314e4b933a6f37
Jane Costa <yberdysh@gmail.com>
2018-03-20 15:31:18 -0400

    finished design

--- src/index.js
+++ src/index.js
@@ -15,13 +15,34 @@ const map = new mapboxgl.Map({
 
 // style: 'mapbox://styles/mapbox/light-v9'
 
-const markerDomEl = document.createElement("div");
-markerDomEl.style.width = "32px";
-markerDomEl.style.height = "39px";
-markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
-new mapboxgl.Marker(markerDomEl).setLngLat([-74.009, 40.705]).addTo(map);
+// const markerDomEl = document.createElement("div");
+// markerDomEl.style.width = "32px";
+// markerDomEl.style.height = "39px";
+// markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+// new mapboxgl.Marker(markerDomEl).setLngLat([-74.009, 40.705]).addTo(map);
 
 
 
-const marker = buildMarker("activity", [-74.509151, 40.005086]); // or [-87.6354, 41.8885]
-marker.addTo(map);
+
+buildMarker("home", [-74.009, 40.705]).addTo(map); // or [-87.6354, 41.8885]
+// marker.addTo(map);
+buildMarker("activity", [-73.998552, 40.714008]).addTo(map); //chinatown fun center
+buildMarker("activity", [-73.977622, 40.761433]).addTo(map); //museum of modern art
+buildMarker("activity", [-73.990964, 40.733317]).addTo(map); //strand bookstore
+buildMarker("activity", [-74.0445, 40.689249]).addTo(map); //statue of liberty
+buildMarker("activity", [-73.851666, 40.747151]).addTo(map); //hall of science museum
+buildMarker("activity", [-73.985758, 40.748541]).addTo(map); //empire state building
+buildMarker("activity", [-73.845062, 40.746384]).addTo(map); //unisphere
+buildMarker("restaurant", [-73.851666, 40.726622]).addTo(map); //stix
+buildMarker("restaurant", [-73.851666, 40.726622]).addTo(map); //mile end
+buildMarker("restaurant", [-73.896733, 40.705414]).addTo(map); //las margaritas
+buildMarker("restaurant", [-73.915778, 40.772775]).addTo(map); //bohemian hall and beer garden
+buildMarker("restaurant", [-73.986404, 40.757194]).addTo(map); //bubba gump shrimp
+buildMarker("restaurant", [-73.973505, 40.751119]).addTo(map); //sushi yasuda
+buildMarker("restaurant", [-73.829057, 40.759373]).addTo(map); //new world mall food court
+buildMarker("restaurant", [-73.911748, 40.767557]).addTo(map); //egyptian coffee shop
+buildMarker("hotel", [-73.981577, 40.769061]).addTo(map); //trump international hotel and tower
+buildMarker("hotel", [-73.899913, 40.741278]).addTo(map); //queens hotel
+buildMarker("hotel", [-74.009137, 40.712923]).addTo(map); //four seasons hotel downtown
+
+
--- src/marker.js
+++ src/marker.js
@@ -1,14 +1,23 @@
 const mapbox = require("mapbox-gl");
 
+mapbox.accessToken = 'pk.eyJ1IjoieWFrdWJvdmE5MiIsImEiOiJjamV6dHJjYTcwZjE4MndubnJiazE2cTNyIn0.tHbarGVvITCNjGItAaeYiw';
+
 const iconURLs = {
-    hotels: "http://i.imgur.com/D9574Cu.png",
-    restaurants: "http://i.imgur.com/cqR6pUI.png",
-    activities: "http://i.imgur.com/WbMOfMl.png"
+    home: "url(https://image.flaticon.com/icons/svg/780/780291.svg)",
+    hotel: "url(https://image.flaticon.com/icons/svg/235/235813.svg)",
+    restaurant: "url(https://image.flaticon.com/icons/svg/235/235830.svg)",
+    activity: "url(https://image.flaticon.com/icons/svg/235/235861.svg)"
 };
 
+
+
 const buildMarker = function (type, coords) {
-    // Your Code Here
-    new mapboxgl.Marker(type).setLngLat(coords).addTo(map);
+  const markerDomEl = document.createElement("div");
+markerDomEl.style.width = "32px";
+markerDomEl.style.height = "32px";
+markerDomEl.style.backgroundImage = iconURLs[type];
+return new mapbox.Marker(markerDomEl).setLngLat(coords);
+    // new mapboxgl.Marker(type).setLngLat(coords).addTo(map);
 };
 
 module.exports = buildMarker;
